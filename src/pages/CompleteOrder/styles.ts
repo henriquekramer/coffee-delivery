050a58737bb6ef57d1e6a4fca5cd42a0aa5c7001
Henrique Kramer <henriquemkramer@gmail.com>
2022-07-21 13:49:44 -0300

    Finalização OrderConfirmed Page

--- src/pages/CompleteOrder/styles.ts
+++ src/pages/CompleteOrder/styles.ts
@@ -5,6 +5,7 @@ export const CompleteOrderContainer = styled.form`
   display: flex;
   justify-content: space-between;
   gap: 2rem;
+  margin-bottom: 9.8rem;
 `
 
 export const SectionBaseStyle = styled.div`
